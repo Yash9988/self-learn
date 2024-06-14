3138ef9933087b69dac766a34d197b6897e4c763
Yash Srivastava <u7349463@anu.edu.au>
2024-06-14 22:21:25 +1000

    Solutions for the q1 and q2

---
+++ PE/p1.py
@@ -0,0 +1,23 @@
+##
+# https://projecteuler.net/problem=1
+
+sum([i for i in range(1, 1000) if i % 3 == 0 or i % 5 == 0])  # 233168
+
+
+##
+# https://projecteuler.net/problem=2
+
+def fib_sum(a=1, b=2, sum_=2):
+    if (c := a + b) > 4e6:
+        return sum_
+
+    sum_ += c if c % 2 == 0 else 0
+
+    return fib_sum(b, c, sum_)
+
+
+print(fib_sum())  # 4613732
+
+##
+# https://projecteuler.net/problem=3
+
