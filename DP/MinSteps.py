0c097d332c674d7f224f30d06614ab479ef9daf2
Yash Srivastava <yashsrivastava1998@gmail.com>
2024-08-20 20:23:09 +1000

    Reorganised code and added comments

--- DP/MinSteps.py
+++ DP/MinSteps.py
@@ -5,31 +5,33 @@ Divide by 2, Divide by 3 or Reduce by 1.
 import timeit
 
 
+# Top-Down approach (Recursive)
 def reduce(n):
     if n == 1:
         return 0
 
     if dp.get(n, -1) == -1:
 
-        s1 = s2 = float('inf')
+        s1 = 1 + reduce(n / 2) if n % 2 == 0 else float('inf')  # Divide by 2
+        s2 = 1 + reduce(n / 3) if n % 3 == 0 else float('inf')  # Divide by 3
+        s3 = 1 + reduce(n - 1)                                  # Reduce by 1
 
-        if n % 2 == 0:
-            s1 = 1 + reduce(n / 2)
-        if n % 3 == 0:
-            s2 = 1 + reduce(n / 3)
-
-        s3 = 1 + reduce(n - 1)
-
-        dp[n] = min(s1, s2, s3)
+        dp[n] = min(s1, s2, s3)                                 # Find min
 
     return dp[n]
 
 
+# Bottom-Up approach (Iterative)
 def reduce_iter(n):
     dp[1], i = 0, 2
 
     while i <= n:
-        dp[i] = 1 + min(dp[i - 1], dp[i / 2] if i % 2 == 0 else float('inf'), dp[i / 3] if i % 3 == 0 else float('inf'))
+        s1 = dp[i / 2] if i % 2 == 0 else float('inf')  # Divide by 2
+        s2 = dp[i / 3] if i % 3 == 0 else float('inf')  # Divide by 3
+        s3 = dp[i - 1]                                  # Reduce by 1
+
+        dp[i] = 1 + min(s1, s2, s3)                     # Find min
+
         i += 1
     return dp[n]
 
