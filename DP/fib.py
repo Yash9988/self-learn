cceb5578bfd0f81128f189402f40b11c1cd31df5
Yash Srivastava <yashsrivastava1998@gmail.com>
2024-08-20 12:24:27 +1000

    Impleted iterative dp for fib

--- DP/fib.py
+++ DP/fib.py
@@ -2,6 +2,7 @@
 import timeit
 from collections import defaultdict
 
+
 # Sloppy ol' recursion. Too repetitive
 def fib(n):
     if n == 0 or n == 1:
@@ -10,20 +11,35 @@ def fib(n):
     return fib(n - 1) + fib(n - 2)
 
 
-# Chad DP. Extremely Efficient
+# Chad DP (with memoization). Time efficient
 def fib_op(n):
     if n == 0 or n == 1:
         return n
-    elif dp[n] == -1:
+    elif dp.get(n, -1) == -1:
         dp[n] = fib_op(n - 1) + fib_op(n - 2)
 
     return dp[n]
 
 
-dp = defaultdict(lambda: -1)
+# Chad iterative DP. Space & Time Efficient
+def fib_op2(n):
+    a, b = 0, 1
+    i = 1
 
+    while i < n:
+        a, b = b, a + b
+        i += 1
+
+    return b
+
+
+dp = {}
+
+# timeit python fib.py
 # print(fib(40))  # ~35.64s
 # print(fib_op(40))  # ~0.0s
+# print(fib_op2(40))  # ~0.0s
 
-print(f'{timeit.timeit("fib(20)", number=100, globals=globals()) / 100: .5g}s')
-print(f'{timeit.timeit("fib_op(20)", number=100, globals=globals()) / 100: .5g}s')
+# n = 100
+# print(f'{timeit.timeit("fib(20)", number=n, globals=globals()) / n: .5g}s')
+# print(f'{timeit.timeit("fib_op(20)", number=n, globals=globals()) / n: .5g}s')
