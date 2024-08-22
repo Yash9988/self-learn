a1d4e96745f8dcc9824ca74ef04eb114220ce3d1
Yash Srivastava <yashsrivastava1998@gmail.com>
2024-08-23 04:44:06 +1000

    Implemented basic functionalities for a (min) heap-queue data structure

--- algo-imp/heap.py
+++ algo-imp/heap.py
@@ -1,25 +1,61 @@
-# Implementation for (Min) Heap data structure
+"""
+Implementation for (Min) Heap data structure
+
+IMPORTANT: The indexing begins with ONE(1) to implement the algorithm seamlessly!!!
+"""
+
+
 class Heap:
     def __init__(self):
-        self.heap = []
-        self.last = 0
+        self.heap = {}      # Initialise heap as a dict
+        self.curr = 1       # curr points to the next available index
+
+    # Method to swap nodes if child is smaller than parent (bottom to top)
     def swap_up(self, idx):
-        if idx - 1:
-            curr, parent = self.heap[idx - 1], self.heap[(idx // 2) - 1]
-            if curr < parent:
-                self.heap[idx - 1], self.heap[(idx // 2) - 1] = parent, curr
-                self.swap_up(idx // 2)
+        if idx - 1:                                                 # Proceed if not the root index
+            curr, parent = self.heap[idx], self.heap[idx // 2]
+            if curr < parent:                                       # Swap child and parent node
+                self.heap[idx], self.heap[idx // 2] = parent, curr
+                self.swap_up(idx // 2)                              # Recursively swap until order is restored
+
+    # Method to swap nodes if parent is larger than child (top to bottom)
     def swap_down(self, idx=1):
-        min_idx = min(enumerate([self.heap[(2 * idx) - 1], self.heap[2 * idx]]), key=lambda x: x[1])[0]
-        parent, min_child = self.heap[idx - 1], self.heap[(2 * idx) - 1 + min_idx]
-        if min_child < parent:
-            self.heap[idx - 1], self.heap[(2 * idx) - 1 + min_idx] = min_child, parent
-            self.swap_down((2 * idx) - 1 + min_idx)
+        if idx < self.curr and 2 * idx < self.curr:
+            child1, child2 = self.heap.get(2 * idx, float('inf')), self.heap.get(2 * idx + 1, float('inf'))
+            children = enumerate([child1, child2])                                # Get child nodes
+            min_idx = min(children, key=lambda x: x[1])[0]                        # Obtain child-node with min. value
+            parent, min_child = self.heap[idx], self.heap[2 * idx + min_idx]
+            if min_child < parent:                                                # Swap parent and child node
+                self.heap[idx], self.heap[2 * idx + min_idx] = min_child, parent
+                self.swap_down(2 * idx + min_idx)                   # Recursively swap until order is restored
+
+    # Insert new element into the heap
     def push(self, n):
-        self.heap.append(n)
-        self.last += 1
-        self.swap_up(self.last)
+        self.heap[self.curr] = n        # Insert element at the bottom of the heap ("array")
+        self.swap_up(self.curr)         # Check with the parent-node to restore the heap logic order
+        self.curr += 1                  # Increment the curr-pointer
+
+    # Remove (and return) the root node from the heap
     def pop(self):
-        self.heap[0], self.heap[self.last] = self.heap[self.last], self.heap[0]
-        self.last -= 1
-        self.swap_up()
+        if self.curr - 1:
+            self.curr -= 1                  # Decrement to point at the last node of heap
+            self.heap[1], self.heap[self.curr] = self.heap[self.curr], self.heap[1]  # Swap the root node with last node
+            temp = self.heap[self.curr]
+            del self.heap[self.curr]
+            self.swap_down()                # Check with children node to restore heap logic order
+            return temp
+        else:
+            return "The heap is empty"
+
+    def __str__(self):      # TODO: Add proper string representation (using a tree structure, preferably)
+        return f"{[self.heap[i] for i in range(1, self.curr)]}"
+
+
+h = Heap()
+
+# for a in [50, 30, 20]:
+#     h.push(a)
+#
+# print(h)
+# print(h.pop())
+# print(h)
