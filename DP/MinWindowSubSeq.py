b7a82c7c3c0826c82dd48c7c4989c78a958f8b28
Yash Srivastava <yashsrivastava1998@gmail.com>
2024-09-09 22:15:20 +1000

    Implemented TD & BU solutions for the MinWindowSubSeq probelem

--- DP/MinWindowSubSeq.py
+++ DP/MinWindowSubSeq.py
@@ -4,29 +4,32 @@ minimized.
 """
 
 
+# Top-Down Approach
 def MinWindowSubSeq(arr: list, window: int) -> int:
-    if (N := len(arr)) <= window:                       # Check if array is equal (or less) than the window size
-        return min(arr)                                 # Minimum sum is the min-element
+    if (n := len(arr)) <= window:                       # Check if array is equal (or less) than the window size
+        dp[n] = arr[-1]                                 # Store value of last element under DP (indexed by array size)
+        return min(arr)                                 # Minimum sum is the min-element of array
 
-    selected = [0 for _ in range(N)]                    # Set flag for each element to track selection
-    min_sum = 0                                         # Initialise variable to hold the min-sum
+    elif n not in dp:                                   # Check if solution in DP
+        sub_ = [MinWindowSubSeq(arr[:-i], window) for i in range(1, window + 1)]    # Recursively obtain sub-solutions
+        dp[n] = arr[-1] + min(sub_)                     # Update the DP with the min-sum for the particular array
+        return min([dp[n - i] for i in range(window)])  # Return the min-result from the final array window
 
-    for i in range(window - 1, N):                      # Start from the end element of the first window
-        min_, idx, new = arr[i], i, 1                   # Initialise variables to track the min-element
-
-        for j in range(1, window):                      # Iterate through the window in reverse
-            if selected[i - j]:                         # Check if any element from the window is already selected
-                new = 0                                 # Flag no new element being selected in the current window
-                break                                   # Break out of the loop
-            elif arr[i - j] < min_:                     # Check if any element in window is smaller than current min
-                min_, idx = arr[i - j], i - j           # Update the respective tracking variable
-
-        if new:                                         # Check if any new element was selected from the window
-            selected[idx] = 1                           # Update the element's flag as 1 (True)
-            min_sum += min_                             # Add element's value to the min-sum variable
-
-    return min_sum                                      # Return the result
+    return dp[n]                                        # Return the DP value for min-sum at index
 
 
+# Bottom-Up Approach
+def MinWindowSubSeq_(arr: list, window: int) -> int:
+    if (N := len(arr)) <= window:                   # Check if array is equal (or less) than the window size
+        return min(arr)                             # Minimum sum is the min-element
+
+    for i in range(window, N):                      # Iterate from the outside of the first window
+        arr[i] += min(arr[i - window: i])           # Iterative obtain the min-sum for each sub-problem
+
+    return min(arr[-window:])                       # Return the min-result from the final array window
+
+
+dp = {}
 x = [3, 2, 1, 1, 2, 3, 1, 3, 2, 1]
 print(MinWindowSubSeq(x, 3))
+print(MinWindowSubSeq_(x, 3))
