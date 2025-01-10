b644ef614b266fb35be3e9c3f513d1833a4de22b
Yash Srivastava <yashsrivastava1998@gmail.com>
2025-01-10 09:44:48 +0530

    [D] Completed the '916. Word Subsets' problem.

---
+++ leetcode/916.py
@@ -0,0 +1,44 @@
+"""
+916. Word Subsets [MEDIUM]
+
+You are given two string arrays words1 and words2.
+
+A string b is a subset of string a if every letter in b occurs in a including multiplicity.
+
+    - For example, "wrr" is a subset of "warrior" but is not a subset of "world".
+
+A string a from words1 is universal if for every string b in words2, b is a subset of a.
+
+Return an array of all the universal strings in words1. You may return the answer in any order.
+
+
+Example 1:
+Input: words1 = ["amazon","apple","facebook","google","leetcode"], words2 = ["e","o"]
+Output: ["facebook","google","leetcode"]
+
+
+Example 2:
+Input: words1 = ["amazon","apple","facebook","google","leetcode"], words2 = ["l","e"]
+Output: ["apple","google","leetcode"]
+
+
+Constraints:
+
+    -> 1 <= words1.length, words2.length <= 10^4
+    -> 1 <= words1[i].length, words2[i].length <= 10
+    -> words1[i] and words2[i] consist only of lowercase English letters.
+    -> All the strings of words1 are unique.
+
+
+Concepts: Max occurrence dict
+"""
+from collections import Counter
+
+
+# Optimised Solution
+def wordSubsets(words1: list[str], words2: list[str]) -> list[str]:
+    count = Counter()                                               # Dict to track max-freq of chars in the list
+    for word in words2:                                             # Iterate through the list
+        count |= Counter(word)                                      # Update the dict with max-char-occurrence
+
+    return [word for word in words1 if not count - Counter(word)]   # Return the result
