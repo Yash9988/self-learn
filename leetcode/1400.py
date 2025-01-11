f399a978995d8d54cfd335e7338b1fee167f2112
Yash Srivastava <yashsrivastava1998@gmail.com>
2025-01-11 06:07:40 +0530

    [D] Completed the '1400. Construct K Palindrome Strings' problem.

---
+++ leetcode/1400.py
@@ -0,0 +1,48 @@
+"""
+1400. Construct K Palindrome Strings [MEDIUM]
+
+
+Given a string s and an integer k, return true if you can use all the characters in s to construct k palindrome strings
+or false otherwise.
+
+
+Example 1:
+Input: s = "annabelle", k = 2
+Output: true
+
+Explanation: You can construct two palindromes using all characters in s.
+Some possible constructions "anna" + "elble", "anbna" + "elle", "anellena" + "b"
+
+
+Example 2:
+Input: s = "leetcode", k = 3
+Output: false
+
+Explanation: It is impossible to construct 3 palindromes using all the characters of s.
+
+
+Example 3:
+Input: s = "true", k = 4
+Output: true
+
+Explanation: The only possible solution is to put each character in a separate string.
+
+
+Constraints:
+
+    -> 1 <= s.length <= 10^5
+    -> s consists of lowercase English letters.
+    -> 1 <= k <= 10^5
+
+
+Concepts: Palindromes
+
+
+Explanation: https://leetcode.com/problems/construct-k-palindrome-strings/solutions/563379/java-c-python-straight-forward
+"""
+from collections import Counter
+
+
+# Optimised Solution
+def canConstruct(s: str, k: int) -> bool:
+    return sum(1 & i for i in Counter(s).values()) <= k <= len(s)   # Check for condition and return the result
